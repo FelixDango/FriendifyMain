d8e9a1126d75a19e10c473084bee24a667f2cbf6
FelixDango <dangofelix@gmail.com>
2023-05-15 20:22:56 +0200

    (clientApp) feat: add profile component and add some styles to login and register components

--- FriendifyMain/ClientApp/src/app/components/profile/profile.component.ts
+++ FriendifyMain/ClientApp/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SexEnum, StatusEnum, User} from "../../models/user";
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
-
+  user: User = {
+    id: 1,
+    username: 'john_doe',
+    firstName: 'John',
+    lastName: 'Doe',
+    password: 'password',
+    birthDate: new Date('1990-01-01'),
+    email: 'john.doe@example.com',
+    suspended: false,
+    sex: SexEnum.Male,
+    status: StatusEnum.Single,
+    picture: null,
+    country: 'USA',
+    phoneNumber: '+1 1234567890',
+    follows: [],
+    followedBy: [],
+    posts: [],
+    address: '123 Main St',
+    city: 'New York',
+    isModerator: false,
+    isAdmin: false
+  };
 }
