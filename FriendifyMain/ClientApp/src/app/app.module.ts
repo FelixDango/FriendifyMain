9f039db5ae6f5f403287d7a4de31f8e5ed9c851c
FelixDango <dangofelix@gmail.com>
2023-05-31 13:54:41 +0200

    (FrontEnd) Add post compose component and update navigation

--- FriendifyMain/ClientApp/src/app/app.module.ts
+++ FriendifyMain/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfilePageComponent } from './views/profile-page/profile-page.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ComposePostComponent } from './components/compose-post/compose-post.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     LoginComponent,
     RegisterComponent,
     ProfilePageComponent,
-    ProfileComponent
+    ProfileComponent,
+    ComposePostComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ FriendifyMain/ClientApp/src/app/components/compose-post/compose-post.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compose-post',
+  templateUrl: './compose-post.component.html',
+  styleUrls: ['./compose-post.component.scss']
+})
+export class ComposePostComponent {
+  selectedFile: File | null = null;
+  text: string = '';
+  characterCount: number = 0;
+  isOverLimit: boolean = false;
+  postContent: string = '';
+  errorMessage: string = '';
+
+  handleFileInput(event: any) {
+    const file: File = event.target.files[0];
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'video/mp4', 'video/avi'];
+
+    if (file && allowedTypes.includes(file.type)) {
+      this.selectedFile = file;
+      this.errorMessage = '';
+    } else {
+      this.selectedFile = null;
+      this.errorMessage = 'Invalid file format. Only images (JPEG, PNG, GIF) and videos (MP4, AVI) are allowed.';
+    }
+  }
+
+  clearFile() {
+    this.selectedFile = null;
+    this.errorMessage = '';
+  }
+
+  handleTextInput(event: any): void {
+    this.characterCount = this.text.length;
+    this.isOverLimit = this.characterCount > 281;
+    if (!this.isOverLimit) this.text = event.target.value;
+  }
+
+  submitPost() {
+    // Perform post submission logic here
+    // You can access the entered text using a template reference variable or ngModel
+    // The selected file can be accessed via this.selectedFile
+    console.log('Text:', this.text);
+    console.log('Selected File:', this.selectedFile);
+  }
+}
