1e42f74872ab59986a4d67709c9565a04e05f799
FelixDango <dangofelix@gmail.com>
2023-06-05 18:06:58 +0200

    (FrontEnd) fix: Auth Service missed observe

--- FriendifyMain/ClientApp/src/app/services/auth.service.ts
+++ FriendifyMain/ClientApp/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,14 +9,14 @@ export class AuthService {
   private baseUrl = 'https://localhost:7073/api/Account';
 
   constructor(private http: HttpClient) {}
-  login(username: string, password: string, rememberMe: boolean): Observable<any> {
+  login(username: string, password: string, rememberMe: boolean): Observable<HttpResponse<any>> {
     const loginData = {
       username: username,
       password: password,
       rememberMe: rememberMe
     };
 
-    return this.http.post(`${this.baseUrl}/login`, loginData);
+    return this.http.post<any>(`${this.baseUrl}/login`, loginData, { observe: 'response' });
   }
 
 
