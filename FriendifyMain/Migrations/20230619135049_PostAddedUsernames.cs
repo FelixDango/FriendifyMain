195c51ea59ad2ee0b307b9f5de4a854a15d5bfab
FadyGeo <70844715+FadyGeo@users.noreply.github.com>
2023-06-19 15:52:40 +0200

    (Backend) Posts model: Added Username field

---
+++ FriendifyMain/Migrations/20230619135049_PostAddedUsernames.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FriendifyMain.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostAddedUsernames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Posts");
+        }
+    }
+}
--- FriendifyMain/Models/Post.cs
+++ FriendifyMain/Models/Post.cs
@@ -9,6 +9,7 @@ namespace FriendifyMain.Models
         public int Id { get; set; }
 
         public int UserId { get; set; }
+        public string Username { get; set; }
         [JsonIgnore]
         public User User { get; set; }
         public string Content { get; set; }
